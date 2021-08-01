569b390cd9f55f95443746719957a9dad60ebcd2
Vijay Guttula <ssvijayg@gmail.com>
2021-08-01 16:22:55 +0530

    todo init

--- routes/ApiRouter.js
+++ routes/ApiRouter.js
@@ -6,37 +6,27 @@ const bcrypt = require('bcrypt');
 // user login
 apiRouter.post('/auth/login', async (req, res) => {
   const { email, password } = req.body;
-  // authentication
-  await User.findOne({ email }).exec(async (error, found) => {
-    if (error) {
-      res.status(500).json({
-        for: 'email',
-        message:
-          "'No user with the entered email is present, please create your account'",
-      });
-      return;
-    }
-    if (!found) {
-      res.status(501).json({
-        for: 'email',
-        message:
-          "'No user with the entered email is present, please create your account'",
-      });
-      return;
-    }
+  const findUser = await User.findOne({ email });
+  console.log(findUser);
+  if (findUser) {
     const checkPassword = await bcrypt.compareSync(password, findUser.password);
-    if (!checkPassword) {
-      res.status(500).json({
-        for: 'password',
-        message: 'Wrong password entered',
+    if (checkPassword) {
+      res.status(200).json({
+        message: 'User login successfull',
+        data: findUser,
       });
       return;
     }
-    res.status(200).json({
-      message: 'User login successfull',
-      data: findUser,
+    res.status(500).json({
+      for: 'password',
+      message: 'Wrong password entered',
     });
     return;
+  }
+  res.status(501).send({
+    for: 'email',
+    message:
+      "'No user with the entered email is present, please create your account'",
   });
 });
 
--- src/Pages/ToDo.jsx
+++ src/Pages/ToDo.jsx
@@ -1,7 +1,82 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import AppRouter from '../apis/AppRouter';
+import { AppContext } from '../context/AppContext';
 
 const ToDo = () => {
-  return <div>Todo</div>;
+  const [todos, setTodos] = useState([]);
+  const [newTodoText, setNewTodoText] = useState('');
+  const { credentials } = useContext(AppContext);
+  let history = useHistory();
+
+  if (!credentials) {
+    window.alert('Login to get started');
+    history.push('/');
+  }
+
+  const updateBackendTodos = async () => {
+    try {
+      const response = await AppRouter.post('/todos', {
+        todos,
+      });
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    if (!newTodoText) return;
+    setTodos([...todos, { checked: false, text: newTodoText }]);
+    setNewTodoText('');
+  };
+
+  const toggleTodo = (index) => {
+    const newTodoList = [...todos];
+    newTodoList[index].checked = !newTodoList[index].checked;
+    setTodos(newTodoList);
+  };
+
+  // to fetch the todos data on load
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await AppRouter.get('/todos');
+        console.log(response);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    // fetchData()
+  }, []);
+
+  return (
+    <div className='container'>
+      <div className='todo'>
+        {todos && <h1>Woah! You got quite a list</h1>}
+        {!todos && <h1>Add down your tasks</h1>}
+        {todos &&
+          todos.map((todo, id) => {
+            return (
+              <div key={id}>
+                <input type='checkbox' onChange={() => toggleTodo(id)} />
+                <label htmlFor=''>{todo.text}</label>
+                <br />
+              </div>
+            );
+          })}
+        <form onSubmit={addTodo}>
+          <input
+            type='text'
+            value={newTodoText}
+            onChange={(e) => setNewTodoText(e.target.value)}
+          />
+          <button type='submit'>Add</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default ToDo;
