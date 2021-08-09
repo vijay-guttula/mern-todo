4992f4ae1a7c46f2de77ed28caca1b1f0a9ddcaa
Vijay Guttula <ssvijayg@gmail.com>
2021-08-09 06:14:04 +0530

    prod

--- client/src/apis/AppRouter.js
+++ client/src/apis/AppRouter.js
@@ -2,7 +2,10 @@ import axios from 'axios';
 require('dotenv').config();
 
 export default axios.create({
-  baseURL: `http://localhost:3000/api/v1/to-do`,
+  baseURL:
+    process.env.NODE_ENV !== 'production'
+      ? 'http://localhost:3000/api/v1/to-do'
+      : 'https://mern-stack-todo1.herokuapp.com/api/v1/to-do',
   headers: {
     'Content-type': 'application/json',
   },
